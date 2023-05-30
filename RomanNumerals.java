d9ac313e79e6f19000218ee7011bc7489b2f0ee6
Eduardo Eismann <eduardo.eismann@gmail.com>
2023-05-30 17:46:59 -0300

    JOptionPane added to start

--- RomanNumerals.java
+++ RomanNumerals.java
@@ -2,17 +2,36 @@ import converters.ConvertValueToArabic;
 import converters.ConvertValueToRoman;
 import interfaces.ArabicRoman;
 
+import javax.swing.*;
+
 public class RomanNumerals {
     public static void main( String args[] ){
         ConvertValueToRoman convertToRoman = new ConvertValueToRoman();
         ConvertValueToArabic convertToArabic = new ConvertValueToArabic();
 
-        System.out.println(convertToRoman.convertValue( new ArabicRoman(1984) ).getRoman() );
-        System.out.println(convertToRoman.convertValue( new ArabicRoman(2000) ).getRoman() );
-        System.out.println(convertToRoman.convertValue( new ArabicRoman(2023) ).getRoman() );
+        int option = 1;
 
-        System.out.println(convertToArabic.convertValue( new ArabicRoman("MCMLXXXIV") ).getArabic() );
-        System.out.println(convertToArabic.convertValue( new ArabicRoman("MM") ).getArabic() );
-        System.out.println(convertToArabic.convertValue( new ArabicRoman("MMXXIII") ).getArabic() );
+        while(option != 0) {
+            String select = JOptionPane.showInputDialog(
+                "1 - Roman to Arabic\n" +
+                "2 - Arabic to Roman\n" +
+                "0 - Exit"
+            );
+            option = toInt(select);
+
+            if(option == 1){
+                String roman = JOptionPane.showInputDialog("Type numeral in Roman");
+                Integer returnArabic = convertToArabic.convertValue( new ArabicRoman( roman.toUpperCase() ) ).getArabic();
+                JOptionPane.showMessageDialog(null, returnArabic);
+            } else if(option == 2) {
+                String arabic = JOptionPane.showInputDialog("Type numeral in Arabic");
+                String returnRoman = convertToRoman.convertValue( new ArabicRoman( toInt(arabic) ) ).getRoman();
+                JOptionPane.showMessageDialog(null, returnRoman);
+            }
+        }
+    }
+
+    private static int toInt(String number) {
+        return Integer.parseInt(number);
     }
 }
